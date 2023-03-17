8fb1b0261ee889df9fdd05bfadc31c2d3f4f8ae5
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2023-03-17 10:44:03 -0700

    userHome

--- src/components/UserHome.js
+++ src/components/UserHome.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate} from "react-router-dom"
+import { useNavigate} from "react-router-dom";
+import axios from "axios"
+import UserNavBar from "./UserNavBar";
 const UserHome =()=>{
     const navigate = useNavigate();
     const [id, setId] = useState(null);
     const [name, setName] = useState(null);
-
+    const [coaches, setCoaches] = useState([]);
     const logout = ()=>{
         sessionStorage.clear();
         navigate('/userlogin');
@@ -18,13 +20,38 @@ const UserHome =()=>{
         }else{
             setId(id);
             setName(name);
+            async function fetchCoaches() {
+                const res = await axios.get("http://localhost:8080/coaches");
+                console.log(res.data);
+                setCoaches(res.data);
+                
+              }
+            fetchCoaches();
         }
     }, []);
 
     return(
         <>
+
+        <UserNavBar></UserNavBar>
         <h1>Welcome {name}  </h1>
-        <button onClick={logout}>Logout</button>
+        <div className="container2">
+        
+        {coaches.map(({name, mobileNumber, speciality,id })=>(
+            <div class="card">
+            <div class="card-body">
+                
+              <h5 class="card-title">{name}</h5>
+              <h5 class="card-title">Coach Id:  {id}</h5>
+              <h5 class="card-title">Mobile Number:  {mobileNumber}</h5>
+              <h5 class="card-title">Specialty: {speciality}</h5>
+              <button>Book an Appointment</button>
+            </div>
+          </div>
+          
+        ))}
+        </div>
+        
         </>
     )
      
---
+++ src/components/UserNavBar.js
@@ -0,0 +1,42 @@
+import { useNavigate } from "react-router-dom";
+import React from "react";
+
+function UserNavBar() {
+    const navigate = useNavigate();
+    const logout = () => {
+        sessionStorage.clear();
+        navigate("/coachlogin");
+      };
+  return (
+    <>
+      <nav class="navbar bg-primary" data-bs-theme="dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            CareFreeHealth
+          </a>
+          <li className="navbar nav-item">
+            <a className="contact" href="/userappointments">
+              My Appointments
+            </a>
+          </li>
+          <li className="navbar nav-item">
+            <a className="contact" href="/userprofile">
+              View Profile
+            </a>
+          </li>
+          <li className="navbar nav-item">
+            <a className="contact" href="/contact">
+              Contact Us!
+            </a>
+          </li>
+          <li className="navbar nav-item">
+            <a href="/userlogin" className="contact" onClick={logout}>
+              Logout
+            </a>
+          </li>
+        </div>
+      </nav>
+    </>
+  );
+}
+export default UserNavBar;
