0421d0a9ca478e603df9d9d033022bef967d4950
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2023-03-28 11:16:32 -0700

    cssOnUserSched

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ function Home(){
         <h1 className="home">Unlock your full potential and become your best self!</h1>
         <div className="card-container">
         <div class="card2" >
-  <img src="https://png.pngitem.com/pimgs/s/42-423085_clip-art-person-clip-art-black-man-symbol.png" class="card-img-top" alt="..."/>
+  
   <div class="card-body2">
     <Link to="userlogin" class="btn btn-primary">Login as a User </Link>
     
@@ -18,7 +18,7 @@ function Home(){
   </div>
 </div>
 <div class="card2" >
-  <img  src="https://png.pngitem.com/pimgs/s/72-721152_transparent-coach-clipart-coach-png-png-download.png" class="card-img-top" alt="..."/>
+  
   <div class="card-body2">
     
     <Link to="coachlogin" class="btn btn-primary">Login as a Coach</Link>
--- src/components/UserSchedule.js
+++ src/components/UserSchedule.js
@@ -134,10 +134,70 @@ const UserSchedule = () => {
       <>
       
         <UserNavBar></UserNavBar>
+        
         <div className="container2">
           
           <h1 className="userhome">{name}'s Appointments </h1>
+          
         </div>
+        
+        {update ? 
+          <form>
+          <div className="container2 px-1 py-5 mx-auto">
+          
+              <div class="container-fluid px-1 py-5 mx-auto">
+    <div class="row d-flex justify-content-center">
+        <div class="card">
+        <div class="row d-flex justify-content-between mx-2 px-3 card-strip">
+                <div class="left d-flex flex-column">
+                
+                    <h5 class="mb-1">Reschedule Appointment with {findCoachName(appToBeUpdated.coachId)}  </h5>
+                    <div className="right">
+                    <img alt="" src="https://previews.123rf.com/images/wannawit/wannawit2001/wannawit200100012/137138620-doctor-medical-cartoon-design-vector.jpg"/>
+                      </div>
+
+                </div>
+                <input
+              type="date"
+
+              onChange={(e) => {setAppToBeUpdated({...appToBeUpdated, appointmentDate: e.target.value})}}
+              className="form-control"
+              value={appToBeUpdated.appointmentDate}
+            ></input>
+            </div>
+            <div class="row d-flex justify-content-between mx-2 px-3 card-strip">
+                <div class="left d-flex flex-column">
+                <label for="times">Choose a Time Slot:</label>
+            <br></br>
+            <select onChange={(e)=>{setAppToBeUpdated({...appToBeUpdated, slot: e.target.value})}} value={appToBeUpdated.slot} name="times" id="times">
+            <option value="">          </option>
+              <option value="9AM to 10AM">9AM to 10AM</option>
+              <option value="10AM to 11AM">10AM to 11AM</option>
+              <option value="11AM to 12PM">11AM to 12PM</option>
+              <option value="12PM to 1PM">12PM to 1PM</option>
+              <option value="1PM to 2PM">1PM to 2PM</option>
+              <option value="2PM to 3PM">2PM to 3PM</option>
+              <option value="4PM to 5PM">4PM to 5PM</option>
+            </select>
+                    
+                    <p class="text-muted mb-1 sm-text">{}</p>
+                    
+                </div>
+                
+            </div>
+            
+            
+            <div class="row d-flex justify-content-between mx-2 px-3">
+                <button onClick={()=>setUpdate(false)} class="btn btn-white">Go Back</button>
+                <button onClick={(e)=>{updateBooking(e)}} class="btn btn-purple">Reschedule</button>
+            </div>
+        </div>
+    </div>
+</div>
+          </div>
+        </form>
+        :null
+  }
         
         <div className="container2 px-1 py-5 mx-auto">
           {appointments.map(
@@ -168,8 +228,8 @@ const UserSchedule = () => {
             
             
             <div class="row d-flex justify-content-between mx-2 px-3">
-                <button onClick={()=>{findBooking(id)}} class="btn btn-white">Reschedule</button>
-                <button onClick={()=>areYouSure(id)} class="btn btn-purple">Cancel</button>
+                <button onClick={()=>areYouSure(id)} className="btn btn-white">Cancel</button>
+                <button onClick={()=>{findBooking(id)}}  className="btn btn-purple">Reschedule</button>
             </div>
         </div>
     </div>
@@ -177,47 +237,7 @@ const UserSchedule = () => {
               
             )
           )}
-          {update ? 
-          <form>
-          <div className="form-group">
-          <input type="hidden" value={appToBeUpdated.userId} className="form-control"></input>
-          <input type="hidden" value={appToBeUpdated.coachId} className="form-control"></input>
-        </div>
-          <div className="form-group">
-            <label>
-              Date of Appointment <span className="errmsg"></span>
-            </label>R
-            <input
-              type="date"
-
-              onChange={(e) => {setAppToBeUpdated({...appToBeUpdated, appointmentDate: e.target.value})}}
-              className="form-control"
-              value={appToBeUpdated.appointmentDate}
-            ></input>
-          </div>
           
-          <div className="form-group">
-            <label for="times">Choose a Time Slot:</label>
-            <br></br>
-            <select onChange={(e)=>{setAppToBeUpdated({...appToBeUpdated, slot: e.target.value})}} value={appToBeUpdated.slot} name="times" id="times">
-            <option value="">          </option>
-              <option value="9AM to 10AM">9AM to 10AM</option>
-              <option value="10AM to 11AM">10AM to 11AM</option>
-              <option value="11AM to 12PM">11AM to 12PM</option>
-              <option value="12PM to 1PM">12PM to 1PM</option>
-              <option value="1PM to 2PM">1PM to 2PM</option>
-              <option value="2PM to 3PM">2PM to 3PM</option>
-              <option value="4PM to 5PM">4PM to 5PM</option>
-            </select>
-            <br></br>
-            <br></br>
-            <button onClick={(e)=>{updateBooking(e)}} type="button" class="btn btn-primary">
-                        Update
-                      </button>
-          </div>
-        </form>
-        :null
-  }
         
           <Footer></Footer>
         </div>
